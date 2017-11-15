16a7444e5527c93a58592f39a5f3c030445c4929
Mohammed Saqib <msaqib3@gatech.edu>
2017-11-15 00:45:49 -0500

    added function to do by subject_id categorization

--- categorization.py
+++ categorization.py
@@ -11,6 +11,7 @@ def getQSofaCategorization(hadm_ids = None, subject_ids = None):
     """
     This function uses the quick sepsis organ failure assessment, as defined by the third international
     consensus for sepsis
+    TODO: Complete this later
     :param hadm_ids a list of hadm_ids which to apply standard to
     :param subject_ids a list of subject_ids which to apply standard to. if hadm_ids is set, this doesn't do anything
     :return dataframe with hadm_ids as index and a column with sepsis
@@ -35,10 +36,34 @@ def getQSofaCategorization(hadm_ids = None, subject_ids = None):
                                             + commonDB.convertListToSQL(hadm_ids))
     #TODO: Complete this later
     return None
+def getCategorizationsBySubjectID(catDF = None):
+    """
+    This is a function to return the categorizations by subject instead of hadm_id
+    in other words, if a subject had a sepsis diagnosis ever, he/she will be classfied as such
+    :param optional categorization DataFrame to use that uses hadm_id based categorization
+    :return the angus categorization
+    """
+    if catDF == None:
+        conn = commonDB.getConnection()
+        with open("data/sql/angus.sql") as f:
+            query = f.read()
+            query = query.replace("<INSERT IDS HERE>", "")
+        catDF = pd.read_sql(query, conn)
+        catDF.set_index(["hadm_id"], inplace=True)
+    bySubjectCat = []
+    for subject_id in catDF["subject_id"].unique():
+        onlySubject = catDF[catDF["subject_id"] == subject_id]
+        if onlySubject.apply(lambda row: row["angus"] == 1, axis = 1).any():
+            bySubjectCat.append(pd.DataFrame({"subject_id": [subject_id], "angus": [1]}))
+        else:
+            bySubjectCat.append(pd.DataFrame({"subject_id": [subject_id], "angus": [0]}))
+    bySubject = pd.concat(bySubjectCat)
+    return bySubject.set_index("subject_id")
+                
+    
 def getCategorizations(hadm_ids = None):
     """
     This is a function to run the Angus.sql query, which is responsible for categorizing patients
-    :postcondition stores results as data/rawdatafiles/classifiedAngusSepsis.p
     :param ids the hadm_ids to look at when doing the query; if None, run it on all
     :return the angusData
     """
@@ -53,5 +78,3 @@ def getCategorizations(hadm_ids = None):
     angusData = pd.read_sql(query, conn)
     angusData.set_index(["hadm_id"], inplace=True)
     return angusData
-if __name__ == "__main__":
-    getCategorizations(True)
