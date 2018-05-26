0cb682add3e15d673e7eeb183585f64f63f46c88
Mohammed Saqib <msaqib3@gatech.edu>
2018-05-26 13:58:23 -0400

    fixed issue with corrupt file?

--- analyze_waveforms.py
+++ analyze_waveforms.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 from matplotlib.pyplot import plot, show, savefig, xlim, figure, \
                 hold, ylim, legend, boxplot, setp, axes
 
-__n_workers = 1
+__n_workers = 24
 threshold = 1 # threshold is which section of the subjects to consider for a single variable
 numericMapping = pd.read_csv("preprocessing/resources/numeric_waveform_to_variable_map.csv")
 numericMapping["numeric"] = numericMapping["numeric"].str.upper()
@@ -23,7 +23,7 @@ numericMapping["high_level_var"] = numericMapping["high_level_var"].str.upper()
 def processSubjectID(subject_id):
     reader = WaveformReader(numericMapping=numericMapping)
     reader.traverser.numeric = True
-    toReturn = []
+    toReturn = [pd.DataFrame()]
     fileToHADMID = reader.traverser.matchWithHADMID(subject_id)
     multiRecords = reader.traverser.getMultiRecordFiles(subject_id)
     if len(multiRecords) == 0:
@@ -34,21 +34,24 @@ def processSubjectID(subject_id):
         singleRecord["SUBJECT_ID"] = subject_id
         if fileToHADMID[multiRecord] != "NOT FOUND": #could not be matched for some reason
             admittime = read_sql("SELECT ADMITTIME from ADMISSIONS WHERE HADM_ID = " + str(fileToHADMID[multiRecord])).iloc[0, 0]
-        data, fields = reader.getRecord(subject_id, multiRecord)
-        for sig_name in data.columns:
-            if (~(data[sig_name].apply(np.isnan))).all():
-                singleRecord[sig_name + "_PERCENT_MISSING"] = 0
-            else:
-                singleRecord[sig_name + "_PERCENT_MISSING"] = pd.isnull(data[sig_name]).value_counts()[True] / len(data)
-            #For each signal, find the percentage that is filled in the first 24 hours after admission
-            if fileToHADMID[multiRecord] != "NOT FOUND":
-                admittime = pd.Timestamp(admittime)
-                firstDay = data.iloc[data.index < admittime + pd.Timedelta("1 days")]
-                if (pd.isnull(firstDay[sig_name])).all():
-                    singleRecord[sig_name + "_PERCENT_MISSING_FIRST_24_HOURS"] = 1
+        try:
+            data, fields = reader.getRecord(subject_id, multiRecord)
+            for sig_name in data.columns:
+                if (~(data[sig_name].apply(np.isnan))).all():
+                    singleRecord[sig_name + "_PERCENT_MISSING"] = 0
                 else:
-                    singleRecord[sig_name + "_PERCENT_MISSING_FIRST_24_HOURS"] =  1 - pd.isnull(firstDay[sig_name]).value_counts()[False] / (60*60*24*60)
-        singleRecord["length"] = len(data)
+                    singleRecord[sig_name + "_PERCENT_MISSING"] = pd.isnull(data[sig_name]).value_counts()[True] / len(data)
+                #For each signal, find the percentage that is filled in the first 24 hours after admission
+                if fileToHADMID[multiRecord] != "NOT FOUND":
+                    admittime = pd.Timestamp(admittime)
+                    firstDay = data.iloc[data.index < admittime + pd.Timedelta("1 days")]
+                    if (pd.isnull(firstDay[sig_name])).all():
+                        singleRecord[sig_name + "_PERCENT_MISSING_FIRST_24_HOURS"] = 1
+                    else:
+                        singleRecord[sig_name + "_PERCENT_MISSING_FIRST_24_HOURS"] =  1 - pd.isnull(firstDay[sig_name]).value_counts()[False] / (60*60*24*60)
+            singleRecord["length"] = len(data)
+        except:
+            singleRecord["comment"] = "Could not getRecord"
         toReturn.append(singleRecord)
     return pd.concat(toReturn)
 
@@ -58,7 +61,7 @@ def helperWaveformRunner(toRunQueue, toReturnQueue):
     Uses queues to analyze waveforms for key prelim stats
     '''
     for subject_id in iter(toRunQueue.get, None):
-        print(subject_id)
+        print(toRunQueue.qsize())
         toReturn = processSubjectID(subject_id)
         toReturnQueue.put(toReturn)
 
@@ -83,7 +86,7 @@ if __name__ == "__main__":
     manager = Manager()
     inQueue = manager.Queue()
     outQueue = manager.Queue()
-    subjects = reader.traverser.getSubjects()[5000:]
+    subjects = reader.traverser.getSubjects()
     [inQueue.put(subject) for subject in subjects]
     [inQueue.put(None) for i in range(__n_workers)]
     processes = [Process(target=helperWaveformRunner, args=(inQueue, outQueue)) for i in range(__n_workers)]
